d7287245fb12700fcdcd9e1a2faff67cdf4efdd0
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-10-08 11:34:52 +0200

    Oprettet a klasser og kopieret ra gammel projekt

--- VitecMVC/Controllers/HomeController.cs
+++ VitecMVC/Controllers/HomeController.cs
@@ -25,5 +25,15 @@ namespace VitecMVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Produkter()
+        {
+            return View();
+        }
     }
 }
