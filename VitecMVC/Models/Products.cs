5be7dbe41f616d84b0f356315c281074329299fc
Anders Madsen <ande48d0@edu.eal.dk>
2019-11-12 17:48:37 +0100

    API med produkt på

---
+++ VitecMVC/Models/Products.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitecMVC.Models
+{
+    public class Products
+    {
+    [JsonProperty("id")]
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("title")]
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("productDescription")]
+        public string ProductDescription
+    {
+            get;
+            set;
+        }
+
+        [JsonProperty("price")]
+        public string Price
+        {
+            get;
+            set;
+        }
+
+    }
+}
